c54b926a57481519590aee95cb1d78492a1e8f64
mark-labuschagne <marklabuschagne7@gmail.com>
2022-10-21 16:01:33 +0200

    check for register mediator in entrypoint
    PLAT-591

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from "express";
 
-import { getConfig } from './config/config'
+import { getConfig, REGISTER_MEDIATOR } from './config/config'
 import logger from './logger'
 import routes from './routes'
+import { mediatorSetup } from "./openhim/openhim";
 
 const port = getConfig().port
 const app = express()
 
-app.use(express.json({type: 'application/fhir+json'}))
+app.use(express.json({ type: 'application/fhir+json' }))
 
 app.use('/', routes)
 
 app.listen(port, () => {
   logger.info(`Server is running on port - ${port}`)
+
+  if (REGISTER_MEDIATOR) {
+    mediatorSetup()
+  }
 })
