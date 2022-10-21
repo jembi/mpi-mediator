4b4b1bcffae4d487813afd020cd7ec39885ab637
mark-labuschagne <marklabuschagne7@gmail.com>
2022-10-21 16:00:51 +0200

    openhim mediator setup function
    PLAT-591

---
+++ src/openhim/openhim.ts
@@ -0,0 +1,50 @@
+import fs from 'fs'
+import path from 'path'
+
+import logger from '../logger'
+import { MediatorConfig } from '../types/mediatorConfig'
+import { RequestOptions } from '../types/request'
+import { registerMediator } from '../utils/register'
+import { fetchConfig, activateHeartbeat } from '../utils/heartbeat'
+import { OPENHIM_PASSWORD, OPENHIM_MEDIATOR_URL, OPENHIM_USERNAME, TRUST_SELF_SIGNED } from '../config/config'
+
+export const mediatorSetup = () => {
+    const mediatorConfigFile = fs.readFileSync(path.resolve(path.resolve(), 'mediatorConfig.json'))
+
+    let mediatorConfig: MediatorConfig
+    try {
+        mediatorConfig = JSON.parse(JSON.stringify(mediatorConfigFile))
+    } catch (error) {
+        logger.error(`Failed to parse JSON in mediatorConfig.json`)
+        throw error
+    }
+
+    const openhimConfig: RequestOptions = {
+        apiURL: OPENHIM_MEDIATOR_URL,
+        password: OPENHIM_PASSWORD,
+        username: OPENHIM_USERNAME,
+        trustSelfSigned: TRUST_SELF_SIGNED,
+        urn: mediatorConfig.urn
+    }
+
+    registerMediator(openhimConfig, mediatorConfig, (error) => {
+        if (error) {
+            logger.error(`Failed to register mediator with: ${error.message}`)
+            throw error
+        }
+
+        logger.info('Successfully registered mediator!')
+
+        fetchConfig(openhimConfig, (err) => {
+            if (err) {
+                logger.error(`Failed to fetch initial config: ${error}`)
+                throw err
+            }
+
+            const emitter = activateHeartbeat(openhimConfig)
+            emitter.on('error', (err2) => {
+                logger.error(`Heartbeat failed: ${JSON.stringify(err2)}`)
+            })
+        })
+    })
+}
