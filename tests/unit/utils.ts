23ab143a5abbafed3d5ba28ba33efec549702f31
bradsawadye <bradsawadye@gmail.com>
2022-10-27 13:39:19 +0200

    Add unit tests for functions used in the validation
    PLAT-562

---
+++ tests/unit/utils.ts
@@ -0,0 +1,69 @@
+import { expect } from 'chai';
+import nock from 'nock';
+
+import { getConfig } from '../../src/config/config';
+import { buildOpenhimResponseObject, postData } from '../../src/routes/utils';
+import { OpenHimResponseObject, PostResponseObject } from '../../src/types/response';
+
+const config = getConfig();
+
+describe('Utils', () : void => {
+  describe('*buildOpenhimResponseObject', () : void => {
+    it('should return Object', () : void => {
+      const transactionStatus : string = 'Success';
+      const httpStatus : number = 200;
+      const body : object = {
+        message: 'Success'
+      };
+      const contentType : string = 'application/json';
+      
+      const returnedObect : OpenHimResponseObject = buildOpenhimResponseObject(
+        transactionStatus,
+        httpStatus,
+        body,
+        contentType
+      );
+
+      expect(returnedObect['x-mediator-urn']).to.equal(config.mediatorUrn);
+      expect(returnedObect.status).to.equal(transactionStatus);
+      expect(returnedObect.response).to.have.property('timestamp');
+      expect(returnedObect.response.headers).to.deep.equal({
+        'content-type': contentType
+      });
+      expect(returnedObect.response.body).to.deep.equal(body);
+    });
+  });
+
+  describe('*postData', () : void => {
+    it('should fail to post when service being posted to is down', async () : Promise<void> => {
+      const response : PostResponseObject = await postData('test', 2000, '', 'application/json', 'data')
+
+      expect(response.status).to.equal(500);
+      expect(response.body).to.have.property('error');
+    });
+
+    it('should post data', async () : Promise<void> => {
+      const host : string = 'example';
+      const port : number = 3000;
+      const path : string = 'fhir';
+      const contentType : string = 'application/json';
+      const data = JSON.stringify({
+        data: 'data'
+      });
+      const dataReturned : object = {
+        message: 'Success'
+      };
+
+      nock(`http://${host}:${port}`)
+        .post(`/${path}`)
+        .reply(200, {
+          message: 'Success'
+        });
+      
+      const response : PostResponseObject = await postData(host, port, path, contentType, data);
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(dataReturned);
+    });
+  });
+});
---
+++ tests/unit/validation.ts
@@ -0,0 +1,73 @@
+import { expect } from 'chai';
+import nock from 'nock';
+
+import { getConfig } from '../../src/config/config';
+import { validate } from '../../src/routes/handlers/validation';
+import { ValidateResponseObect } from '../../src/types/response';
+
+const config = getConfig();
+
+describe('Validation handler', () : void => {
+  describe('*validate', () : void => {
+    it('should return error when validating server is unavalaible', async () : Promise<void> => {
+      const bundle = {
+        resourceType: 'Bundle',
+        id: 'testBundle',
+        type: 'transaction',
+        entry: [
+          {
+            fullUrl: 'Patient/testPatient',
+            resource: {
+              resourceType: 'Patient',
+              id: '123',
+              tr: 12
+            },
+            request: {
+              method: 'PUT',
+              url: 'Patient/testPatient'
+            }
+          }
+        ]
+      };
+      nock(`http://${config.fhirDatastoreHost}:${config.fhirDatastorePort}`)
+      .post('/fhir/Bundle/$validate')
+      .reply(500, {});
+
+      const result : ValidateResponseObect = await validate(bundle);
+
+      expect(result.status).to.equal(500);
+    });
+
+    it('should succesfully validate', async () : Promise<void> => {
+      const bundle = {
+        resourceType: 'Bundle',
+        id: 'testBundle',
+        type: 'transaction',
+        entry: [
+          {
+            fullUrl: 'Patient/testPatient',
+            resource: {
+              resourceType: 'Patient',
+              id: '123',
+              tr: 12
+            },
+            request: {
+              method: 'PUT',
+              url: 'Patient/testPatient'
+            }
+          }
+        ]
+      };
+
+      nock(`http://${config.fhirDatastoreHost}:${config.fhirDatastorePort}`)
+        .post('/fhir/Bundle/$validate')
+        .reply(200, {
+          message: 'Success'
+        });
+
+      const result : ValidateResponseObect = await validate(bundle);
+      
+      expect(result.status).to.equal(200);
+    });
+  });
+});
