fb0cc100e52840615997d298011f5254c3d6938f
bradsawadye <bradsawadye@gmail.com>
2022-10-27 13:26:38 +0200

    Add the config used in sending data to fhir server

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,9 @@ export const getConfig = () => {
   return Object.freeze({
     port: process.env.SERVER_PORT || 3000,
     logLevel: process.env.LOG_LEVEL || 'info',
-    registerMediator: !!process.env.REGISTER_MEDIATOR
-  })
-}
+    fhirDatastoreHost: process.env.FHIR_DATASTORE_HOST || 'hapi-fhir',
+    fhirDatastorePort: process.env.FHIR_DATASTORE_PORT || 8080,
+    mediatorUrn: process.env.MEDIATOR_URN || 'urn:mediator:mpi-mediator',
+    runningMode: process.env.MODE || ''
+  });
+};
