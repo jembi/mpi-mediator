c0ff909817e243922ac0c7c9b16555a40062851f
bradsawadye <bradsawadye@gmail.com>
2022-11-01 14:58:28 +0200

    Add functions for retrieving a patient resource and id and, modifying the bundle
    The functions will be used in extracting a patient from the bundle and then creating the patient in the Client Registry and then storing the clinical data in the FHIR datastore with the Client Registry external patient ref
    
    PLAT-563

--- src/routes/utils.ts
+++ src/routes/utils.ts
@@ -2,7 +2,9 @@ import moment from 'moment';
 import fetch from 'node-fetch';
 
 import { getConfig } from '../config/config';
+import logger from "../logger";
 import { OpenHimResponseObject, PostResponseObject, Response } from '../types/response';
+import { Bundle, Resource, Entry } from '../types/bundle';
 
 const config = getConfig();
 
@@ -59,4 +61,69 @@ export const buildOpenhimResponseObject = (
     status: openhimTransactionStatus,
     response
   };
+};
+
+export const extractPatientResource = (bundle : Bundle) :  Resource | null => {
+  if (!bundle || !bundle.entry || !bundle.entry.length) {
+    return null;
+  };
+
+  const patientEntry : Entry | undefined = bundle.entry.find((val, i) => {
+    if (val.resource.resourceType === 'Patient') {
+      return true;
+    }
+  });
+
+  return patientEntry ? patientEntry.resource : null;
+};
+
+export const extractPatientId = (bundle: Bundle) : string | null => {
+  const patientRefs : string[] = Array.from(
+    new Set(JSON.stringify(bundle).match(/Patient\/[^/^"]*/g))
+  );
+
+  if (!patientRefs.length) {
+    return null;
+  };
+  const splitRef : string[] = patientRefs[0].split('/');
+
+  return splitRef.length === 2 ? splitRef[1] : null
+};
+
+/*
+  This method modifies the bundle by replacing the temporary patient reference with the Client
+  Registry's patient reference, after creating the patient resource in the Client Registry.
+  It also adds the request property to the bundle entries. For bundles of type 'document'.
+  The Patient resouce is also removed from the bundle. Only clinical data is stored in the Fhir Datastore
+*/
+export const modifyBundle = (
+  bundle: Bundle,
+  tempPatientRef: string,
+  clientRegistryPatientRef: string
+) : Bundle => {
+  let modifiedBundle = Object.assign({}, bundle);
+
+  if (modifiedBundle.type === 'document') {
+    logger.info('Converting document bundle to transaction bundle');
+    modifiedBundle.type = 'transaction';
+  };
+
+  const newEntry = modifiedBundle.entry.filter(val => val.resource.resourceType !== 'Patient').map(entry => {
+    return Object.assign(
+      {}, entry,
+      {
+        request: {
+          method: 'PUT',
+          url: `${entry.resource.resourceType}/${entry.resource.id}`
+        }
+      }
+    );
+  });
+  modifiedBundle.entry = newEntry;
+
+  modifiedBundle = JSON.parse(
+    JSON.stringify(modifiedBundle).replace(new RegExp(tempPatientRef, 'g'), clientRegistryPatientRef)
+  )
+
+  return modifiedBundle;
 };
