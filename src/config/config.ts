4b5762b76232b5d90ef9b1c175c77eae7013f744
bradsawadye <bradsawadye@gmail.com>
2022-11-02 13:06:03 +0200

    Add the client registry auth request config
    THese are the variables used in getting auth token from client registry

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,13 @@ export const getConfig = () => {
     fhirDatastorePort: process.env.FHIR_DATASTORE_PORT || 8080,
     fhirDatastoreProtocol: process.env.FHIR_DATASTORE_PROTOCOL || 'http', 
     mediatorUrn: process.env.MEDIATOR_URN || 'urn:mediator:mpi-mediator',
+    clientRegistryHost: process.env.CLIENT_REGISTRY_HOST || 'santempi',
+    clientRegistryProtocol: process.env.CLIENT_REGISTRY_PROTOCOL || 'http',
+    clientRegistryPort: process.env.CLIENT_REGISTRY_PORT || 8080,
+    clientRegistryAuthPath: process.env.CIENT_REGISTRY_AUTH_PATH || '/auth/oauth2_token',
+    clientRegistryAuthCredentials: process.env.CLIENT_REGISTRY_AUTH_CREDENTIALS || `grant_type=client_credentials&scope=*&client_id='DISI CLIENT'&client_secret=DISI&resource=oath2_token`,
+    clientRegistryAuthCredentialsContentType: process.env.CLIENT_REGISTRY_AUTH_CREDENTIALS_CONTENT_TYPE || 'application/x-www-form-urlencoded',
+    clientRegistryAuthHeaderType: process.env.CLIENT_REGISTRY_AUTH_TYPE || 'Bearer',
     runningMode: process.env.MODE || ''
   });
 };
