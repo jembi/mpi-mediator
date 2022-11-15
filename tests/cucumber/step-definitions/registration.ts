8d59fdc77be092651a751775c11bd1a244784b80
Mark-Labuschagne <marklabuschagne7@gmail.com>
2022-11-15 12:26:59 +0200

    more linting

--- tests/cucumber/step-definitions/registration.ts
+++ tests/cucumber/step-definitions/registration.ts
@@ -1,14 +1,14 @@
 // @ts-ignore
-import { Given, When, Then } from "cucumber";
-import { expect } from "chai";
-import path from "path";
-import fetch from "node-fetch";
+import { Given, When, Then } from 'cucumber';
+import { expect } from 'chai';
+import path from 'path';
+import fetch from 'node-fetch';
 import rewire from 'rewire';
-import https from "https";
+import https from 'https';
 
-import { getConfig } from "../../../src/config/config";
-import { MediatorConfig } from "../../../src/types/mediatorConfig";
-import { mediatorSetup } from "../../../src/openhim/openhim";
+import { getConfig } from '../../../src/config/config';
+import { MediatorConfig } from '../../../src/types/mediatorConfig';
+import { mediatorSetup } from '../../../src/openhim/openhim';
 
 process.env.TRUST_SELF_SIGNED = 'true';
 process.env.REGISTER_MEDIATOR = 'true';
@@ -22,16 +22,20 @@ const openhimModule = rewire(path.resolve(__dirname, '../../../src/openhim/openh
 
 const DeleteMediator = async () => {
   const headers = {} as { [key: string]: string };
-  headers.Authorization = 'Basic ' + Buffer.from(config.openhimUsername + ":" + config.openhimPassword).toString('base64');
+  headers.Authorization =
+    'Basic ' +
+    Buffer.from(config.openhimUsername + ':' + config.openhimPassword).toString('base64');
 
-  const resolveMediatorConfig = openhimModule.__get__("resolveMediatorConfig");
+  const resolveMediatorConfig = openhimModule.__get__('resolveMediatorConfig');
 
-  const mediatorConfig: MediatorConfig = resolveMediatorConfig(path.resolve(__dirname, '../../unit/mediatorConfig-test.json'));
+  const mediatorConfig: MediatorConfig = resolveMediatorConfig(
+    path.resolve(__dirname, '../../unit/mediatorConfig-test.json')
+  );
 
   await fetch(config.openhimMediatorUrl + `/mediators/` + mediatorConfig.urn, {
     agent: httpsAgent,
     headers,
-    method: 'DELETE'
+    method: 'DELETE',
   });
 };
 
@@ -41,7 +45,7 @@ function sleep(ms: number) {
   });
 }
 
-Given("the OpenHIM Core service is up and running", async (): Promise<void> => {
+Given('the OpenHIM Core service is up and running', async (): Promise<void> => {
   const response = await fetch(config.openhimMediatorUrl + '/heartbeat', {
     agent: httpsAgent,
   });
@@ -50,24 +54,30 @@ Given("the OpenHIM Core service is up and running", async (): Promise<void> => {
 });
 
 When('the mediatorSetup function is run', () => {
-  expect(() => mediatorSetup(path.resolve(__dirname, '../../unit/mediatorConfig-test.json'))).to.not.throw();
+  expect(() =>
+    mediatorSetup(path.resolve(__dirname, '../../unit/mediatorConfig-test.json'))
+  ).to.not.throw();
 });
 
 Then('the OpenHIM Core service should have a registered mediator', async () => {
   const headers = {} as { [key: string]: string };
-  headers.Authorization = 'Basic ' + Buffer.from(config.openhimUsername + ":" + config.openhimPassword).toString('base64');
+  headers.Authorization =
+    'Basic ' +
+    Buffer.from(config.openhimUsername + ':' + config.openhimPassword).toString('base64');
 
-  const resolveMediatorConfig = openhimModule.__get__("resolveMediatorConfig");
+  const resolveMediatorConfig = openhimModule.__get__('resolveMediatorConfig');
 
   await sleep(1000);
 
   const response = await fetch(config.openhimMediatorUrl + '/mediators', {
     agent: httpsAgent,
-    headers
+    headers,
   });
 
   const respBody: MediatorConfig[] = JSON.parse(JSON.stringify(await response.json()));
-  const mediatorConfig: MediatorConfig = resolveMediatorConfig(path.resolve(__dirname, '../../unit/mediatorConfig-test.json'));
+  const mediatorConfig: MediatorConfig = resolveMediatorConfig(
+    path.resolve(__dirname, '../../unit/mediatorConfig-test.json')
+  );
 
   expect(respBody.length).to.eql(1);
   expect(respBody[0].name).to.eql(mediatorConfig.name);
--- tests/unit/openhim-test.spec.ts
+++ tests/unit/openhim-test.spec.ts
@@ -9,72 +9,78 @@ import { getConfig } from '../../src/config/config';
 chai.config.truncateThreshold = 0;
 
 const expectedMediatorConfig: MediatorConfig = {
-    urn: 'urn:mediator:mpi-mediator',
-    version: '1.0.0',
-    name: 'MPI mediator',
-    description: "A mediator handling interactions between the OpenHIM Core service, Sante MPI, Hapi-FHIR, and Kafka",
-    defaultChannelConfig: [
+  urn: 'urn:mediator:mpi-mediator',
+  version: '1.0.0',
+  name: 'MPI mediator',
+  description:
+    'A mediator handling interactions between the OpenHIM Core service, Sante MPI, Hapi-FHIR, and Kafka',
+  defaultChannelConfig: [
+    {
+      name: 'MPI mediator',
+      urlPattern: '^/fhir$',
+      routes: [
         {
-            name: 'MPI mediator',
-            urlPattern: '^/fhir$',
-            routes: [
-                {
-                    name: "MPI Endpoint",
-                    host: "mpi-mediator",
-                    port: "3000",
-                    primary: true,
-                    type: "http"
-                }
-            ],
-            allow: ["instant"],
-            methods: ["POST"],
-            type: 'http'
-        }
-    ],
-    endpoints: [
-        {
-            name: 'MPI Endpoint',
-            host: 'mpi-mediator',
-            path: '/fhir',
-            port: '3000',
-            primary: true,
-            type: 'http'
-        }
-    ],
-    configDefs: []
+          name: 'MPI Endpoint',
+          host: 'mpi-mediator',
+          port: '3000',
+          primary: true,
+          type: 'http',
+        },
+      ],
+      allow: ['instant'],
+      methods: ['POST'],
+      type: 'http',
+    },
+  ],
+  endpoints: [
+    {
+      name: 'MPI Endpoint',
+      host: 'mpi-mediator',
+      path: '/fhir',
+      port: '3000',
+      primary: true,
+      type: 'http',
+    },
+  ],
+  configDefs: [],
 };
 
 const config = getConfig();
 
 const expectedRequestOptions: RequestOptions = {
-    username: config.openhimUsername,
-    password: config.openhimPassword,
-    apiURL: config.openhimMediatorUrl,
-    trustSelfSigned: config.trustSelfSigned,
-    urn: 'urn:mediator:mpi-mediator'
+  username: config.openhimUsername,
+  password: config.openhimPassword,
+  apiURL: config.openhimMediatorUrl,
+  trustSelfSigned: config.trustSelfSigned,
+  urn: 'urn:mediator:mpi-mediator',
 };
 
 describe('Mediator Registration', () => {
-    const openhimModule = rewire(path.resolve(__dirname, '../../src/openhim/openhim.ts'));
-    const resolveMediatorConfig = openhimModule.__get__("resolveMediatorConfig");
-    const resolveOpenhimConfig = openhimModule.__get__("resolveOpenhimConfig");
+  const openhimModule = rewire(path.resolve(__dirname, '../../src/openhim/openhim.ts'));
+  const resolveMediatorConfig = openhimModule.__get__('resolveMediatorConfig');
+  const resolveOpenhimConfig = openhimModule.__get__('resolveOpenhimConfig');
 
-    describe('Test Resolving of Mediator Config', () => {
-        it('Should parse and return the mediatorConfig', () => {
-            const mediatorConfig: MediatorConfig = resolveMediatorConfig(path.resolve(__dirname, './mediatorConfig-test.json'));
-            expect(mediatorConfig).to.eql(expectedMediatorConfig);
-        });
-        it('Should Return an Error for Non-Existant File', () => {
-            expect(() => resolveMediatorConfig(path.resolve(__dirname, './asdftgsa.json')))
-                .to.throw(new RegExp('Cannot find module'));
-        });
+  describe('Test Resolving of Mediator Config', () => {
+    it('Should parse and return the mediatorConfig', () => {
+      const mediatorConfig: MediatorConfig = resolveMediatorConfig(
+        path.resolve(__dirname, './mediatorConfig-test.json')
+      );
+      expect(mediatorConfig).to.eql(expectedMediatorConfig);
     });
+    it('Should Return an Error for Non-Existant File', () => {
+      expect(() => resolveMediatorConfig(path.resolve(__dirname, './asdftgsa.json'))).to.throw(
+        new RegExp('Cannot find module')
+      );
+    });
+  });
 
-    describe('Test Resolving of Openhim Config', () => {
-        it('Should Return the RequestOptions Matching expectedRequestOptions', () => {
-            const mediatorConfig: MediatorConfig = resolveMediatorConfig(path.resolve(__dirname, './mediatorConfig-test.json'));
-            const openhimConfig: RequestOptions = resolveOpenhimConfig(mediatorConfig.urn);
-            expect(openhimConfig).to.eql(expectedRequestOptions);
-        });
+  describe('Test Resolving of Openhim Config', () => {
+    it('Should Return the RequestOptions Matching expectedRequestOptions', () => {
+      const mediatorConfig: MediatorConfig = resolveMediatorConfig(
+        path.resolve(__dirname, './mediatorConfig-test.json')
+      );
+      const openhimConfig: RequestOptions = resolveOpenhimConfig(mediatorConfig.urn);
+      expect(openhimConfig).to.eql(expectedRequestOptions);
     });
+  });
 });
