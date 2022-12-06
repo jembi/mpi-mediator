bdfef92050a6515127089171849714f570bc6bf2
mark-labuschagne <marklabuschagne7@gmail.com>
2022-12-06 10:09:42 +0200

    renamed hapi-fhir services compose files
    PLAT-627

--- tests/docker/hapi-fhir/compose.sh
+++ tests/docker/hapi-fhir/compose.sh
@@ -13,10 +13,10 @@ ROOT_PATH="${FILE_PATH}/.."
 . "${ROOT_PATH}/utils/utils.sh"
 
 main() {
-  docker-compose -p mediator_tests -f "$FILE_PATH"/docker-compose-postgres.yml up -d
+  docker-compose -p mediator_tests -f "$FILE_PATH"/docker-compose.postgres.yml up -d
   util::await_container_ready postgres-1 running false
 
-  docker-compose -p mediator_tests -f "${FILE_PATH}"/docker-compose.yml up -d
+  docker-compose -p mediator_tests -f "${FILE_PATH}"/docker-compose.hapi-fhir.yml up -d
   util::await_container_ready hapi-fhir running true
 }
 
