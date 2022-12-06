062f36a569ff85f0fae852cae14d701889de20a6
mark-labuschagne <marklabuschagne7@gmail.com>
2022-12-06 10:29:08 +0200

    deployment for openhim test containers
    PLAT-627

---
+++ tests/docker/openhim/compose.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export COMPOSE_IGNORE_ORPHANS=1
+
+FILE_PATH=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+  pwd -P
+)
+readonly FILE_PATH
+
+# shellcheck source=utils.sh
+ROOT_PATH="${FILE_PATH}/.."
+. "${ROOT_PATH}/utils/utils.sh"
+
+main() {
+  docker-compose -p mediator_tests -f "$FILE_PATH"/docker-compose.mongo-db.yml up -d
+  util::await_container_ready mongo-db running false
+
+  docker-compose -p mediator_tests -f "${FILE_PATH}"/docker-compose.openhim-core.yml up -d
+  util::await_container_ready openhim-core running true
+
+  node "$FILE_PATH"/openhimConfig.js
+}
+
+main "$@"
---
+++ tests/docker/openhim/openhimConfig.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const fs = require('fs');
+const https = require('https');
+const fetch = require('node-fetch');
+const path = require('path');
+
+(async function () {
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+
+  const authHeader = new Buffer.from('root@openhim.org:openhim-password').toString('base64');
+
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+  const jsonData = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, 'openhim-import.json'))
+  );
+
+  const data = JSON.stringify(jsonData);
+
+  try {
+    const res = await fetch(`https://localhost:8080/metadata`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+        Authorization: `Basic ${authHeader}`,
+      },
+      agent: httpsAgent,
+      body: data,
+    });
+
+    if (res.status == 401) {
+      throw new Error(`Incorrect OpenHIM API credentials`);
+    }
+
+    if (res.status != 201) {
+      throw new Error(`Failed to import OpenHIM config: ${res.statusText}`);
+    }
+  } catch (error) {
+    console.error(`Failed to import OpenHIM config: ${error}`);
+  }
+})();
