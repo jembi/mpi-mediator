9ef5409d796a3dc1d1589ad9505c3ffe2a4e994a
bradsawadye <bradsawadye@gmail.com>
2022-12-06 10:43:32 +0200

    Update src/routes/handlers/matchPatientSync.ts
    Co-authored-by: Nour Borgi <48720958+nour-borgi@users.noreply.github.com>

--- src/routes/handlers/matchPatientSync.ts
+++ src/routes/handlers/matchPatientSync.ts
@@ -4,7 +4,7 @@ import { MpiMediatorResponseObject } from '../../types/response';
 import { processBundle } from '../../utils/kafkaFhir';
 
 export const matchSyncHandler = async (bundle: Bundle): Promise<MpiMediatorResponseObject> => {
-  logger.info('Fhir bundle recieved for synchronous matching of the patient!');
+  logger.info('Fhir bundle received for synchronous matching of the patient!');
 
   const handlerResponse: MpiMediatorResponseObject = await processBundle(bundle);
 
